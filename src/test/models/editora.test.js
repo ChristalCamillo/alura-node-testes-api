ae0b3b11de740c8c1d6765d45899be98788b0674
ChristalCamillo <christal.b.camillo@gmail.com>
2024-03-11 17:59:46 -0300

    teste salvar no banco de dados editora

--- src/test/models/editora.test.js
+++ src/test/models/editora.test.js
@@ -15,4 +15,31 @@ describe('Testes de unidade para o model Editora', () => {
       expect.objectContaining(objetoEditora),
     );
   });
+
+  // skip é um metodo do jester que pula o teste pra melhorar visualização ou
+  // desenvolver melhor depois
+  it.skip('Deve salvar editora no BD', () => {
+    const editora = new Editora(objetoEditora);
+
+    editora.salvar().then((dados) => {
+      expect(dados.nome).toBe('CDC');
+    });
+  });
+
+  it.skip('Deve salvar no BD usando a sintaxe moderna', async () => {
+    const editora = new Editora(objetoEditora);
+
+    const dados = await editora.salvar();
+
+    const retornado = await Editora.pegarPeloId(dados.id);
+
+    expect(retornado).toEqual(
+      expect.objectContaining({
+        id: expect.any(Number),
+        ...objetoEditora,
+        created_at: expect.any(String),
+        updated_at: expect.any(String),
+      }),
+    );
+  });
 });
